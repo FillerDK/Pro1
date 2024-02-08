355cad37f9468e031851b99aa9438971346803c3
Philip home pc <fillerdk1234@gmail.com>
2024-02-08 21:19:32 +0100

    L04 litteratur

---
+++ L05/src/literatur/B521.java
@@ -0,0 +1,14 @@
+package literatur;
+
+public class B521 {
+    public static void main(String[] args) {
+        int count = 0;
+        while (count < 100) {
+            // Point A (always true)
+            System.out.println("Welcome to Java!");
+            count++;
+            // Point B (sometimes true, sometimes false)
+        }
+        // Point C (always false)
+    }
+}
---
+++ L05/src/literatur/B522.java
@@ -0,0 +1,7 @@
+package literatur;
+
+public class B522 {
+    public static void main(String[] args) {
+        // How many times are the following loop bodies
+    }
+}
